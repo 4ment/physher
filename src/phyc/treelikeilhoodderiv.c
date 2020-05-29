324e8124bd5bf7ad1f20d28b9551518192571554
Mathieu Fourment <m.fourment@gmail.com>
2020-05-29 14:42:49 +1000

    Removed treelikeilhoodderiv

--- src/phyc/treelikeilhoodderiv.c
+++
@@ -1,9 +0,0 @@
-//
-//  treelikeilhoodderiv.c
-//  physher
-//
-//  Created by mathieu on 5/5/20.
-//  Copyright © 2020 Mathieu Fourment. All rights reserved.
-//
-
-#include "treelikeilhoodderiv.h"
